024cf5e8e5f2ded939cd0174c14554ac2c5cbc85
gokulNeoSoft <charugokul.s@neosoftmail.com>
2026-06-07 17:30:02 +0530

    created_crud_simple_text_post

--- app/app.py
+++ app/app.py
@@ -1,7 +1,41 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , HTTPException
+from app.schemas import CreatePost,PostResponse
+from app.db_conn import Post , create_db_and_tables , get_async_session
+from sqlalchemy.ext.asyncio import AsyncSession
+from contextlib import asynccontextmanager
 
-app = FastAPI()
 
-@app.get("/hello-world")
-def hello_world():
-    return {"result" : " hello world !!"}
+
+@asynccontextmanager
+async def lifespan(app : FastAPI):
+    await create_db_and_tables()
+    yield
+    
+app = FastAPI(lifespan = lifespan)
+
+@app.get("/posts")
+def get_all_post(limit : int = None) -> list[PostResponse]:
+    if limit and limit > len(text_posts) :
+        raise HTTPException(status_code=500,detail=f"limit greater than the no of post !, give limit below {len(text_posts)}")
+    if limit:
+        return list(text_posts.values())[:limit]
+    return text_posts
+
+@app.get("/post/{id}")
+def get_post_by_id(id:int) -> PostResponse:
+    if id not in text_posts :
+        raise HTTPException(status_code=404,detail="post not found!")
+    return text_posts.get(id)
+
+
+@app.post("/post")
+def create_post(post : CreatePost) -> PostResponse:
+    new_index = len(text_posts)+1
+    new_post = {   
+        "title":post.title   ,
+        "content":post.content
+    }
+    text_posts[new_index]=new_post
+    return new_post
+
+
---
+++ app/schemas.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+
+class CreatePost(BaseModel):
+    title:str
+    content:str
+    
+
+class PostResponse(BaseModel):
+    title:str
+    content:str
+    
